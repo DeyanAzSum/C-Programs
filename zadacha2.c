ed499a7a81b2d05c3fc7cb397224cbc745eca6fa
DeyanAzSum <deyan662@gmail.com>
2025-02-13 22:04:25 +0200

    Add files via upload

--- zadacha2.c
+++ zadacha2.c
@@ -1,43 +1,24 @@
+/*Направете програма, която да намира най-малко 
+общо кратно на две числа. Пример: a=12, б=18, 
+най-малко общо кратно = 36. Изпълнете първите 
+две стъпки от процеса на компилация на тази 
+програма и качете асембли кода заедно със 
+задачата към заданието.*/
 #include <stdio.h>
-int main(void) {
-    unsigned char lights = 0;
-    while (1) {
-        int izbor;
-        printf("1. Promeni osvetlenieto.\n");
-        printf("2. Veche pusnato osvetlenie.\n");
-        printf("Enter your choice: ");
-        scanf("%d", &izbor);
-        if (izbor == 1) {
-            int staq;
-            printf("Nomer na staq ot 1 do 8: ");
-            scanf("%d", &staq);
-            if (staq < 1 || staq > 8) {
-                printf("Chisloto trqbva da e ot edno do osem.\n");
-                return 1;
-            }
-            unsigned char light = 1 << (staq - 1);
-            lights = lights ^ light;
-            printf("Toggled light in room %d.\n", staq);
-        } 
-        else if (izbor == 2) {
-            printf("The light is on in rooms: ");
-            int vklucheni = 0;
-            for (int i = 1; i < 9; i++) {
-                if ((lights >> i) & 1) {
-                    if (vklucheni)
-                    printf(", ");
-                    printf("%d", i);
-                    vklucheni = 1;
-                }
-            }
-            if (!vklucheni) {
-                printf("None");
-            }
-            printf("\n");
-        } 
-        else {
-            printf("Invalid choice! Please choose 1 or 2.\n");
-        }
+int NOD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
+    return a;
+}
+int NOK(int a, int b) {
+    return (a * b) / NOD(a, b);
+}
+int main() {
+    int a = 4, b = 6;
+    int result = NOK(a, b);
+    printf("%d", result);
     return 0;
 }
